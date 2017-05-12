hendriks73/Darcula

commit b56dd7ab3302addf0d2fc7383da29cdeef7d75c0

JToggleButtons on toolbars look bad on Windows (old-style compound borders)

src/com/bulenkov/darcula/ui/DarculaButtonUI.java
@@ -47,7 +47,7 @@ public class DarculaButtonUI extends BasicButtonUI {
     final Border border = c.getBorder();
     final GraphicsConfig config = GraphicsUtil.setupAAPainting(g);
     final boolean square = isSquare(c);
-    if (c.isEnabled() && border != null && button.isContentAreaFilled()) {
+    if (c.isEnabled() && border != null && button.isContentAreaFilled() && !(c instanceof JToggleButton)) {
       final Insets ins = border.getBorderInsets(c);
       final int yOff = (ins.top + ins.bottom) / 4;
       if (!square) {

src/com/bulenkov/darcula/darcula_windows.properties
@@ -19,7 +19,7 @@ darcula.selectionBackground=4B6EAF
 darcula.selectionForeground=bbbbbb
 
 PopupMenu.border=com.bulenkov.darcula.ui.DarculaPopupMenuBorder
-
+ToolBarUI=com.bulenkov.darcula.ui.DarculaToolBarUI
 MenuBar.border=com.bulenkov.darcula.ui.DarculaMenuBarBorder
 
 ToolBar.floatingBackground=3c3f41

src/com/bulenkov/darcula/ui/DarculaToolBarUI.java
@@ -35,4 +35,8 @@ public class DarculaToolBarUI extends MetalToolBarUI {
     g.setColor(UIManager.getColor("ToolBar.background"));
     g.fillRect(0, 0, c.getWidth(), c.getHeight());
   }
+
+  @Override
+  protected void setBorderToNonRollover(Component c) {
+  }
 }

commit 53fa9236ccdfb53510411b45da99cad8282fcc13

Made sure toggle button states are actually visible under OS X. Was broken by https://github.com/hendriks73/Darcula/commit/b56dd7ab3302addf0d2fc7383da29cdeef7d75c0

src/com/bulenkov/darcula/ui/DarculaButtonUI.java
@@ -47,7 +47,7 @@ public class DarculaButtonUI extends BasicButtonUI {
     final Border border = c.getBorder();
     final GraphicsConfig config = GraphicsUtil.setupAAPainting(g);
     final boolean square = isSquare(c);
-    if (c.isEnabled() && border != null && button.isContentAreaFilled() && !(c instanceof JToggleButton)) {
+    if (c.isEnabled() && border != null && button.isContentAreaFilled()) {
       final Insets ins = border.getBorderInsets(c);
       final int yOff = (ins.top + ins.bottom) / 4;
       if (!square) {